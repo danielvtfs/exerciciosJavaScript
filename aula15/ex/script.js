1e53966ffb1dbc880cc35292817a7103a1282e51
your username <daniel.fernandez@unigranrio.br>
2020-01-24 17:27:21 -0300

    aula 15 e 16

--- aula15/ex/script.js
+++ aula15/ex/script.js
@@ -1,6 +1,13 @@
-const numero = prompt('Digite um número');
+let numero = Number(prompt('Digite um número'));
+
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p> seu número é - 2 é ${numero - 2}</p>`;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${numero} é inteiro: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Com Duas casas decimais: ${numero.toFixed(2)}</p>`;
