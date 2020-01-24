afb90f4f963ffed72c3614180ae6dc3280c7f84a
danielvtfs <danielvtfs@gmail.com>
2020-01-23 21:30:33 -0300

    Aulas e exercicios aula 15

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+
+
+//let num2 = Math.floor(num1); //aredonda para cima
+//let num2 = Math.ceil(num1); // aredonda para baixo
+//let num2 = Math.round(num1); // aredonda para o mais próximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8)); //maior número de uma sequência
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8)); //menor número de uma sequência
+//const aleatorio = Math.random() * (10 - 5) + 5; // Números aleatórios entre 5 e 10
+
+//console.log(num1 ** (1/2) ); // raiz quadrada
+console.log(num1 ** 0.5 ); // raiz quadrada
