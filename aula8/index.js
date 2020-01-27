5549d6315176fe63d514bb22e828ce3f2b08a3ad
your username <daniel.fernandez@unigranrio.br>
2020-01-27 17:44:46 -0300

    Renomeando pastas e adicionando até aula 21

--- aula8/index.js
+++ aula8/index.js
@@ -20,3 +20,35 @@ anoNascimento = 2019 - idade;
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
 console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
 console.log(`${nome} nasceu em ${anoNascimento}.`);
+
+
+function meuEscopo () {
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
+
+  const pessoas = [];
+
+  function recebeEventoForm (evento) {
+    evento.preventDefault();
+
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
+
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
+
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` +
+      `${peso.value} ${altura.value}</p>`;
+  }
+
+  form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
