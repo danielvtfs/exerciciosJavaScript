58a180ee18ce544843cf497722feecfb92a30042
your username <daniel.fernandez@unigranrio.br>
2020-03-30 15:13:08 -0300

    Commit das aulas 46 e 47

---
+++ aula46 - setInterval e setTimeout/index.js
@@ -0,0 +1,21 @@
+function mostraHora() {
+  let data = new Date();
+
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false
+  });
+}
+
+const timer = setInterval(() => {
+  console.log(mostraHora());
+  
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(timer)
+}, 3000);
+
+setTimeout(() => {
+  console.log('teste');
+  
+}, 5000);
