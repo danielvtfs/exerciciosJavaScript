0fc40e7bf2175eb22e4344e2a86e5e966421ada5
your username <daniel.fernandez@unigranrio.br>
2020-04-02 15:58:18 -0300

    Commit aula 55

--- aula53 - Closures/index.js
+++ aula53 - Closures/index.js
@@ -1,4 +1,4 @@
-// Global
+// Um closure (fechamento) é uma função que se "lembra" do ambiente — ou escopo léxico — em que ela foi criada.
 function retornaFuncao(nome) {
   return function(sobrenome) {
     return nome + ' ' + sobrenome;
@@ -17,3 +17,32 @@ console.dir(funcao2);
 
 console.log(funcao('Miranda'),
   funcao2('Oliveira'));
+
+//Escopo léxico
+  // Considere a função abaixo:
+  function init() {
+    var name = "Mozilla";
+    function displayName() {
+      alert(name);
+    }
+    displayName();
+  }
+  init(); 
+
+  /* A função init() cria uma variável local chamada name, e depois define uma função chamada displayName(). displayName() 
+  é uma função aninhada (um closure) — ela é definida dentro da função init(), e está disponivel apenas dentro do corpo daquela função. 
+  Diferente de init(), displayName() não tem variáveis locais próprias, e ao invés disso reusa a variável name declarada na função pai. */
+
+  
+  //Closure
+//Agora considere o seguinte exemplo:
+function makeFunc() {
+  var name = "Mozilla";
+  function displayName() {
+    alert(name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc();
