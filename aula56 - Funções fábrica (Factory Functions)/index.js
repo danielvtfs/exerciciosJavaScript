97137235f7f847195f05088b04ae5aed3cd91c64
your username <daniel.fernandez@unigranrio.br>
2020-04-07 15:59:44 -0300

    Commit da aula 57

--- aula56 - Funções fábrica (Factory Functions)/index.js
+++ aula56 - Funções fábrica (Factory Functions)/index.js
@@ -1,15 +1,37 @@
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome, 
         sobrenome,
+        altura,
+        peso,
+        //getter
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        },
+        //setter
+        set nomeCompleto(string){
+            string = string.split(' ');
+            this.nome = string.shift();
+            console.log(string);
+            this.sobrenome = string.join();
+        },
+
         fala (assunto) {
             return `${nome} está ${assunto}`;
         },
-        peso: 80,
-        altura
 
+        //getter
+        get imc(){
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa('Daniel', 'Fernandez');
-console.log(p1.fala('falando sobre js'));
+const p1 = criaPessoa('Daniel', 'Fernandez', 1.80, 85);
+p1.nomeCompleto = 'Daniel Santana'
+
+console.log(p1.nomeCompleto);
+
+
+
