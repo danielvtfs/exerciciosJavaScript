790f450012b015f4dcc14b06e083b36676be9c17
danielvtfs <danielvtfs@gmail.com>
2020-04-14 19:05:42 -0300

    Commit da aula 76

--- aula76 - Herança/index.js
+++ aula76 - Herança/index.js
@@ -26,9 +26,32 @@ Camiseta.prototype.aumento = function(percent) {
     this.preco = this.preco + (this.preco * (percent / 100))
 }
 
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+          return estoque;
+        },
+        set: function(valor) {
+          if (typeof valor !== 'number') return;
+          estoque = valor;
+        }
+      });
+}
+
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca;
+
+
+
 const produto = new Produto('gen', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'preta');
-camiseta.aumento(10)
+const caneca = new Caneca('caneca', 13, 'plastico', 5);
+
+console.log(caneca);
 console.log(camiseta);
 console.log(produto);
-
