0453bab6972d073bf796ed4eab75ffded409a4cf
danielvtfs <danielvtfs@gmail.com>
2020-05-06 15:31:10 -0300

    Commit das aulas 96 e 97

--- aula96-Criando um gerador de senhas/src/modules/geradores.js
+++ aula96-Criando um gerador de senhas/src/modules/geradores.js
@@ -3,7 +3,23 @@ const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const geraMaiuscula = () => String.fromCharCode(rand(65, 91))
 const geraMinuscula = () => String.fromCharCode(rand(97, 123))
 const geranumero = () => String.fromCharCode(rand(48, 58))
-const simbolos = ',.;~^[]{}=+!@#$%&*()';
+const simbolos = ',.;~^[]{}=+!@#$%&*()-';
 const geraSimbolo = () => simbolos[rand(0, simbolos.length)]
 
-console.log(geraSimbolo());
+export default function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
+    const senhaArray = [];
+    qtd = Number(qtd)
+
+    for (let i = 0; i < qtd; i++) {
+        maiusculas && senhaArray.push(geraMaiuscula());//verifica se maiusculas é true e gera.
+        minusculas && senhaArray.push(geraMinuscula());//verifica se minusculas é true e gera.
+        numeros && senhaArray.push(geranumero());//verifica se numeros é true e gera.
+        simbolos && senhaArray.push(geraSimbolo());//verifica se simbolos é true e gera.
+        
+    }
+
+    return senhaArray.join('').slice(0, qtd);
+    
+}
+
+console.log(geraSenha(10, true, true, true));
---
+++ aula97-Core-js e regenerator-runtime/src/main.js
@@ -0,0 +1,8 @@
+//estes dois imports servem para rodar async/await em navegadores antigos
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import executa from './modules/promises'
+
+import './assets/css/style.css'
+executa();
