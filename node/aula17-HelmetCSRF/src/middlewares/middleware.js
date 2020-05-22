e01225a0e0a80ad56fe57c15761a57c53c2b7774
danielvtfs <danielvtfs@gmail.com>
2020-05-22 19:04:29 -0300

    Commit Node aula 17

---
+++ node/aula17-HelmetCSRF/src/middlewares/middleware.js
@@ -0,0 +1,19 @@
+exports.middleWareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Teste da variável local'
+    next();
+}
+
+exports.outroMiddleWare = (req, res, next) => {
+    next();
+}
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if (err && 'EBADCSRFTOKEN' === err.code) {
+        return res.render('404')
+    }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};
