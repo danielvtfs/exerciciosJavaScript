549981718dd3a89c1e160b17c7ca1c8828d33de8
Daniel Fernandez <danielvtfs@gmail.com>
2020-06-20 22:18:05 -0300

    Criando CRUD de alunos

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Daniel',
-      sobrenome: 'Fernandez',
-      email: 'danielvtfs@gmail.com',
-      idade: '33',
-      peso: 84,
-      altura: 1.8,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
