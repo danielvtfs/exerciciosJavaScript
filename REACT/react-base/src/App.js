a3ee1c6340c74dd6526e61e506b2d7a91d6e107a
Daniel Fernandez <danielvtfs@gmail.com>
2020-06-24 20:59:17 -0300

    Criando modelo de aplicação React

---
+++ REACT/react-base/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ REACT/react-base/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>oie</small>
+      </Title>
+      <Paragrafo>Lorem</Paragrafo>
+      <button type="button">Enviar</button>
+    </Container>
+  );
+}
