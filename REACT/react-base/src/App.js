bebd77032a688871d38a97c7a4bac2a4793c0826
Daniel Fernandez <danielvtfs@gmail.com>
2020-06-25 19:33:56 -0300

    Criando o Redux da aplicação

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-
+import { Provider } from 'react-redux';
+import store from './store';
 import history from './services/history';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
@@ -9,14 +10,14 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyles />
         <ToastContainer autoClose={3000} className="Toast-container" />
       </Router>
-    </>
+    </Provider>
   );
 }
 
--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/action';
 
 export default function Login() {
+  const dispatch = useDispatch();
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.clicaBotao());
+  }
+
   return (
     <Container>
       <Title>
@@ -10,7 +19,9 @@ export default function Login() {
         <small>oie</small>
       </Title>
       <Paragrafo>Lorem</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
