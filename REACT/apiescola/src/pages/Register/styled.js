0212a0773b7e6c4594920a6ade35c5a01ac66a00
Daniel Fernandez <danielvtfs@gmail.com>
2020-06-29 14:08:21 -0300

    Criando tela de cadastro da apiEscola

--- REACT/apiescola/src/pages/Register/styled.js
+++ REACT/apiescola/src/pages/Register/styled.js
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 15px;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 15px;
+  }
+
+  input {
+    height: 30px;
+    padding-left: 7px;
+    border: 1px solid #8e9497;
+    border-radius: 3px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`;
